5200700d68f35463c896f9101d52038b00f4dba5
OleksandrMartyniuk <alexandrmartiniuk@gmail.com>
2017-03-06 00:40:33 +0200

    bad request fix

--- ChatAPI/Roles/RoleBase.cs
+++ ChatAPI/Roles/RoleBase.cs
@@ -1,4 +1,5 @@
-﻿using Core;
+﻿using AuthServer;
+using Core;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -20,7 +21,17 @@ namespace ChatServer.Roles
 
         public virtual void Handle(IClientObject client, string request)
         {
-            RequestObject req = JsonConvert.DeserializeObject<RequestObject>(request);
+            RequestObject req = null;
+            try
+            {
+                req = JsonConvert.DeserializeObject<RequestObject>(request);
+            }
+            catch(Exception e)
+            {
+                LogProvider.AppendRecord(string.Format("user {0} sent bad request {1}", client.Username, request));
+                return;
+            }
+            
             foreach(IHandlerModule module in client.Role.Handlers)
             {
                 if(module.Handle(client, req))
